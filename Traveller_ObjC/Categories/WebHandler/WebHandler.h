50ca4617648e0f6026f433a6d1c3f6de2702ff11
Sagar Shirbhate <sagarshirbhate@sagars-mac.local>
2016-04-06 18:20:18 +0530

    Added Reachability

---
+++ Traveller_ObjC/Categories/WebHandler/WebHandler.h
@@ -0,0 +1,22 @@
+//
+//  WebHandler.h
+//
+//  Created by Sagar Shirbhate on 22/04/15.
+//  Copyright (c) xxxxx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AFNetworking/AFNetworking.h>
+#import "Reachability.h"
+#import <SystemConfiguration/SystemConfiguration.h>
+
+typedef void (^requestCompletionBlock)(NSURLResponse *response, NSDictionary *jsonData, NSError *error);
+@interface WebHandler : NSObject<NSURLConnectionDelegate,NSURLConnectionDataDelegate,NSURLSessionDelegate>
+
+@property(nonatomic,strong) NSMutableDictionary *paramsDictionary;
+
++(id)sharedHandler;
+
+-(void)sendFile:(NSString *)filePath toURL:(NSURL *)url withParameters:(NSDictionary *)params completion:(requestCompletionBlock)completionBlock;
+
+@end
---
+++ Traveller_ObjC/Categories/WebHandler/WebHandler.m
@@ -0,0 +1,110 @@
+//
+//  WebHandler.m
+//
+//  Created by Sagar Shirbhate on 22/04/15.
+//  Copyright (c) xxxxx. All rights reserved.
+//
+
+#import "WebHandler.h"
+#import "AppDelegate.h"
+@implementation WebHandler
+
+#pragma mark call url
+
++(id)sharedHandler
+{
+    static dispatch_once_t token;
+    static id sharedObject;
+    dispatch_once(&token, ^{
+        sharedObject =[[self alloc] init];
+    });
+    return sharedObject;
+}
+-(id)init
+{
+    return self;
+}
+
+- (BOOL)connected
+{
+    Reachability *reachability = [Reachability reachabilityForInternetConnection];
+    NetworkStatus networkStatus = [reachability currentReachabilityStatus];
+    return networkStatus != NotReachable;
+}
+
+
+-(void)sendFile:(NSString *)filePath toURL:(NSURL *)url withParameters:(NSDictionary *)params completion:(requestCompletionBlock)completionBlock
+{
+    if ([self connected])
+    {
+        NSString *boundary = [NSString stringWithFormat:@"Boundary-%@", [[NSUUID UUID] UUIDString]];
+        // configure the request
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+        [request setHTTPMethod:@"POST"];
+        // set content type
+        NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary];
+        [request setValue:contentType forHTTPHeaderField: @"Content-Type"];
+        
+        //create body
+        request.HTTPBody = [self createBodyWithParameters:params paths:@[filePath] boundary:boundary];
+        
+        [NSURLConnection sendAsynchronousRequest:request queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+            NSError *error;
+            NSDictionary* responseJSON = [NSJSONSerialization JSONObjectWithData:data
+                                                                         options:kNilOptions
+                                                                           error:&error];
+            completionBlock(response,responseJSON,error);
+        }];
+        
+    }
+    
+}
+- (NSData *)createBodyWithParameters:(NSDictionary *)parameters paths:(NSArray *)paths boundary:(NSString *)boundary
+{
+    
+    
+    NSMutableData *httpBody = [[NSMutableData alloc] init];
+    
+    // add params (all params are strings)
+    [parameters enumerateKeysAndObjectsUsingBlock:^(NSString *parameterKey, NSString *parameterValue, BOOL *stop)
+     {
+         [httpBody appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+         [httpBody appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", parameterKey] dataUsingEncoding:NSUTF8StringEncoding]];
+         [httpBody appendData:[[NSString stringWithFormat:@"%@\r\n", parameterValue] dataUsingEncoding:NSUTF8StringEncoding]];
+     }];
+    // add image data
+    for (NSString *path in paths)
+    {
+        NSString *filename  = [path lastPathComponent];
+        NSData   *data      = [[NSData alloc] initWithContentsOfFile:path options:NSDataReadingUncached error:nil];
+        NSString *mimetype  = @"png";
+        
+        [httpBody appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+        [httpBody appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"file\"; filename=\"%@\"\r\n", filename] dataUsingEncoding:NSUTF8StringEncoding]];
+        [httpBody appendData:[[NSString stringWithFormat:@"Content-Type: %@\r\n\r\n", mimetype] dataUsingEncoding:NSUTF8StringEncoding]];
+        [httpBody appendData:data];
+        [httpBody appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+        filename =nil;
+    }
+    [httpBody appendData:[[NSString stringWithFormat:@"--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    return httpBody;
+}
+
++(NSString *)mimeTypeForPath:(NSString *)path
+{
+    // get a mime type for an extension using MobileCoreServices.framework
+    
+    CFStringRef extension = (__bridge CFStringRef)[path pathExtension];
+    CFStringRef UTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, extension, NULL);
+    assert(UTI != NULL);
+    
+    NSString *mimetype = CFBridgingRelease(UTTypeCopyPreferredTagWithClass(UTI, kUTTagClassMIMEType));
+    assert(mimetype != NULL);
+    
+    CFRelease(UTI);
+    
+    return mimetype;
+}
+
+@end
