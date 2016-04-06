37d0aca3856b3a232812bcfe85bfb3437159b405
Sagar Shirbhate <sagarshirbhate@sagars-mac.local>
2016-04-06 14:06:04 +0530

    added view

---
+++ Traveller_ObjC/ViewControllers/BasicLogin/BasicLoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  BasicLoginViewController.h
+//  Traveller_ObjC
+//
+//  Created by Sagar Shirbhate on 06/04/16.
+//  Copyright © 2016 Sagar Shirbhate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FBSDKLoginKit.h>
+//import google too
+@interface BasicLoginViewController : UIViewController
+- (IBAction)fb:(id)sender;
+- (IBAction)gmail:(id)sender;
+@end
---
+++ Traveller_ObjC/ViewControllers/BasicLogin/BasicLoginViewController.m
@@ -0,0 +1,42 @@
+//
+//  BasicLoginViewController.m
+//  Traveller_ObjC
+//
+//  Created by Sagar Shirbhate on 06/04/16.
+//  Copyright © 2016 Sagar Shirbhate. All rights reserved.
+//
+
+#import "BasicLoginViewController.h"
+
+@interface BasicLoginViewController ()
+
+@end
+
+@implementation BasicLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)fb:(id)sender {
+}
+
+- (IBAction)gmail:(id)sender {
+}
+@end
